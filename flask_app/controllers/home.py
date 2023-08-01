d21dfdf075bddd6a0efccab6fa1443b8bf52e3d4
peterkim2014 <peterkim2014@gmail.com>
2023-08-01 18:40:43 -0400

    updated

--- flask_app/controllers/home.py
+++ flask_app/controllers/home.py
@@ -1,7 +1,29 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.config.mysqlconnection import MySQLConnection
+from flask_app.models.user import User
 
 @app.route("/")
 def homepage():
     return render_template("home_page.html")
+
+@app.route("/join_waitlist", methods=["POST"])
+def post_waitlist():
+
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"],
+    }
+    email_data = {
+        "first_name": request.form["first_name"],
+        "email": request.form["email"],
+    }
+    if User.validate_registration(data):
+        User.create(data)
+        User.email(email_data)
+        flash("Thank you for joining the waitlist")
+        return redirect("/")
+    else:
+        flash("Please fill in the appropriate information below")
+        return redirect("/")

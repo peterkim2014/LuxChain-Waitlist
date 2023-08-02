f78a811823382d26c97d3870dfb0948e9633a319
peterkim2014 <peterkim2014@gmail.com>
2023-08-02 15:23:51 -0400

    updated

--- flask_app/static/javascript/home_page.js
+++ flask_app/static/javascript/home_page.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var introPos = $('#page-intro').offset().top;
         var introHeight = $('#page-intro').height();
         var appPos = $('#page-app').offset().top;
-        var appHeight = $(window).height() * 3.65; // 369vh
+        var appHeight = $(window).height() * 3.2; // 369vh
         var waitlistPos = $('#page-waitlist').offset().top;
         var waitlistHeight = $('#page-waitlist').height();
 
@@ -18,8 +18,8 @@ $(document).ready(function () {
         else if (scrollPos >= appPos && scrollPos < appPos + appHeight) {
             $('.side-nav-links a').css('color', 'black');
         } 
-        // Check if the page is scrolled at least 25% through the #page-waitlist section
-        else if (scrollPos >= waitlistPos && scrollPos < waitlistPos + waitlistHeight * 0.25) {
+        // Check if the page is scrolled so that at least 50% of the #page-waitlist section is visible
+        else if (scrollPos >= waitlistPos - waitlistHeight * 0.5 && scrollPos < waitlistPos + waitlistHeight * 0.5) {
             $('.side-nav-links a').css('color', 'white');
         } else {
             $('.side-nav-links a').css('color', 'black');

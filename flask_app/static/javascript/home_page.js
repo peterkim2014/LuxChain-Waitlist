76f9f84c8a126d4ed86bfa8b083cf77ad72796bf
peterkim2014 <peterkim2014@gmail.com>
2023-08-02 16:14:32 -0400

    updated

--- flask_app/static/javascript/home_page.js
+++ flask_app/static/javascript/home_page.js
@@ -1,11 +1,41 @@
 $(document).ready(function () {
-    // Define a function to animate the opacity of specified elements
-    function animateOpacity(elements) {
-        elements.stop(true).css('opacity', '0').animate({
-            opacity: '1'
+    var currentSection = '';
+
+    function animateOpacity(section, targetOpacity) {
+        $(section).find('p, h1, h2, h3').stop(true).animate({
+            opacity: targetOpacity
         }, 1000);
     }
 
+    // Initialize the opacity of all sections to 0
+    $('p, h1, h2, h3').css('opacity', '0');
+
+    function checkSection(scrollPos) {
+        var introPos = $('#page-intro').offset().top;
+        var introHeight = $('#page-intro').height();
+        var appPos = $('#page-app').offset().top;
+        var appHeight = $(window).height() * 3.2;
+        var waitlistPos = $('#page-waitlist').offset().top;
+        var waitlistHeight = $('#page-waitlist').height();
+
+        if (scrollPos >= introPos && scrollPos < introPos + introHeight * 0.5) {
+            return '#page-intro';
+        } else if (scrollPos >= appPos && scrollPos < appPos + appHeight) {
+            return '#page-app';
+        } else if (scrollPos >= waitlistPos - waitlistHeight * 0.5 && scrollPos < waitlistPos + waitlistHeight * 0.5) {
+            return '#page-waitlist';
+        } else {
+            return '';
+        }
+    }
+
+    // Determine the current section on page load
+    currentSection = checkSection($(window).scrollTop());
+    if (currentSection) {
+        animateOpacity(currentSection, '1');
+    }
+
+    // Event handlers
     $('a[href*="#page-"]').on('click', function (e) {
         e.preventDefault();
         var targetSection = $(this).attr('href');
@@ -13,34 +43,22 @@ $(document).ready(function () {
 
         $('html, body').animate({
             scrollTop: targetPos
-        }, 1000, function() {
-            animateOpacity($(targetSection).find('p, h1, h2, h3'));
-        });
+        }, 1000);
     });
 
     $(window).on('scroll', function () {
         var scrollPos = $(window).scrollTop();
+        var newSection = checkSection(scrollPos);
 
-        // Get the position and height of each section
-        var introPos = $('#page-intro').offset().top;
-        var introHeight = $('#page-intro').height();
-        var appPos = $('#page-app').offset().top;
-        var appHeight = $(window).height() * 3.2; // 369vh
-        var waitlistPos = $('#page-waitlist').offset().top;
-        var waitlistHeight = $('#page-waitlist').height();
-
-        // Animate opacity for the section that is currently visible
-        if (scrollPos >= introPos && scrollPos < introPos + introHeight * 0.5) {
-            animateOpacity($('#page-intro').find('p, h1, h2, h3'));
-            $('.side-nav-links a').css('color', 'white');
-        } else if (scrollPos >= appPos && scrollPos < appPos + appHeight) {
-            animateOpacity($('#page-app').find('p, h1, h2, h3'));
-            $('.side-nav-links a').css('color', 'black');
-        } else if (scrollPos >= waitlistPos - waitlistHeight * 0.5 && scrollPos < waitlistPos + waitlistHeight * 0.5) {
-            animateOpacity($('#page-waitlist').find('p, h1, h2, h3'));
-            $('.side-nav-links a').css('color', 'white');
-        } else {
-            $('.side-nav-links a').css('color', 'black');
+        // If the active section has changed
+        if (newSection !== currentSection && newSection) {
+            // Animate the opacity to 1 for the new active section
+            animateOpacity(newSection, '1');
+            // Animate the opacity to 0 for the previous active section
+            if (currentSection) {
+                animateOpacity(currentSection, '0');
+            }
+            currentSection = newSection;
         }
     });
 });

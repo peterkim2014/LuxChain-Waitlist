deca8f2a6bd691cbe324a3efac621cc94dab8ecf
peterkim2014 <peterkim2014@gmail.com>
2023-08-04 15:29:06 -0400

    updated

--- flask_app/models/email.py
+++ flask_app/models/email.py
@@ -38,20 +38,23 @@ msg_text = """
         <table class="email" width="84.1%" height="50%" valign="top" cellpadding="0" cellspacing="0" border="0">
             <tr>
                 <td width="750" height="815" background="https://i.imgur.com/EZxrgB9.png">
-                    <table cellpadding="0" cellspacing="0" border="0" margin="auto" >
-                        <tr>
-                            <p class="email-header" style="height: 12%; width: 90%; text-align: end;">Ticket #{number}</p>
-                        </tr>
-                        <tr style="padding-left: 5%;">
-                            <p style="text-align: start; width: 75%; height: 20%;">Congratulations! You are now on the waitlist for LuxChain Wallet Application! Stay tuned for more announcements & updates on the release date.</p>
 
-                            <p style="text-align: start; width: 75%; height: 20%;">LuxChain is a user-friendly application designed to streamline the transaction process, promoting the adoption and growth of blockchain technology. Especially valuable during economic uncertainty, LuxChain ensures users a high degree of privacy and trust. The application provides a decentralized platform for safe and efficient transactions, transforming the way people purchase items.</p>
+                <p class="email-header" style="height: 12%; width: 90%; text-align: end; margin-bottom: 22.5%">Ticket #{number}</p>
 
-                            <p style="text-align: start; width: 75%; height: 20%;">Have any questions about the application? Please visit the website, www.luxchain.io, and fill out the form under contact us!</p>
-                            
-                            <p style="text-align: start; width: 40%; height: 10%;">Our best, <br>LuxChain Team</p>
-                        </tr>
-                    </table>
+                <table cellpadding="0" cellspacing="0" border="0" margin="auto" >
+                    <tr>
+                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; padding-top: 5%;">Congratulations {number}! </p>
+                        
+                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; padding-top: 1%;">You are now on the waitlist for LuxChain Wallet Application! Stay tuned for more announcements & updates on the release date.</p>
+
+                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; padding-top: 1%;">LuxChain is a user-friendly application designed to streamline the transaction process, promoting the adoption and growth of blockchain technology. Especially valuable during economic uncertainty, LuxChain ensures users a high degree of privacy and trust. The application provides a decentralized platform for safe and efficient transactions, transforming the way people purchase items.</p>
+
+                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; padding-top: 1%;">Have any questions about the application? Please visit the website, www.luxchain.io, and fill out the form under contact us!</p>
+                        
+                        <p style="text-align: start; width: 40%; height: 10%; padding-left: 12.5%; padding-top: 1%;">Our best, <br>LuxChain Team</p>
+                    </tr>
+                </table>
+                
                 </td>
             </tr>
         </table>

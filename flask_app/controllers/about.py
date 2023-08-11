55045de95168bfa7554eb2baa075c38df2a6611f
Andrewjkim2035 <andrewjkim2035@gmail.com>
2023-08-11 12:15:30 -0400

    updated mobile

--- flask_app/controllers/about.py
+++ flask_app/controllers/about.py
@@ -1,7 +1,27 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.config.mysqlconnection import MySQLConnection
+import re
+
+def is_mobile(user_agent):
+    # Regular expressions for common mobile device strings
+    mobile_patterns = [
+        "iphone", "ipod", "ipad", "android", "blackberry",
+        "windows phone", "nokia", "samsung", "mobile"
+    ]
+    for pattern in mobile_patterns:
+        if re.search(pattern, user_agent):
+            print(f"Detected mobile device: {user_agent}")
+            return True
+    print(f"Not a mobile device: {user_agent}")
+    return False
 
 @app.route("/about")
 def aboutpage():
-    return render_template("about_page.html")
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if is_mobile(user_agent):
+        return render_template('mobile.about.html')
+    else:
+        return render_template("about_page.html")

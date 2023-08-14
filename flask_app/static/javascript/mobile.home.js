3b27f799d2125236f12e11c4e90a63fbd13dda23
Andrewjkim2035 <andrewjkim2035@gmail.com>
2023-08-14 16:25:16 -0400

    updated

--- flask_app/static/javascript/mobile.home.js
+++ flask_app/static/javascript/mobile.home.js
@@ -6,18 +6,26 @@ swipeContainer.addEventListener('touchstart', (e) => {
     initialX = e.touches[0].clientX;
 });
 
+
+
 swipeContainer.addEventListener('touchmove', (e) => {
     if (initialX === null) return;
 
     const currentX = e.touches[0].clientX;
     const diffX = currentX - initialX;
 
-
     if (diffX < -20) {
         // Swiped right
-        // You can navigate to another HTML file here
-        window.location.href = 'mobile_apppreview';
+        // Add fade-out class
+        document.body.classList.remove('fade-out');
+        document.body.classList.add('fade-in');
+
+        // Delay the actual navigation to allow the transition to play
+        setTimeout(() => {
+            window.location.href = 'mobile_app_preview';
+        }, 100); // 500ms delay to match the 0.5s transition
     }
 
+   
     initialX = null;
 });

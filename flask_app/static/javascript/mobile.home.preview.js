7bff661b07fd82f98ced051db14060b62b4fefe9
Andrewjkim2035 <andrewjkim2035@gmail.com>
2023-08-15 17:08:43 -0400

    updated

--- flask_app/static/javascript/mobile.home.preview.js
+++ flask_app/static/javascript/mobile.home.preview.js
@@ -8,6 +8,45 @@ $('#swipe-container').css('opacity', '0');
 // Upon page load, animate the opacity of these text elements to 1.
 $(document).ready(function() {
     animateTextOpacity('1');
+    
+    $('.hamburger-icon').on('click', function() {
+        // Slide the side nav bar in
+        $('.side-nav-bar').css('transform', 'translateX(0)');
+
+        // Hide the hamburger-icon and show the hamburger-return icon
+        $('.hamburger-return').show();
+    });
+
+    $('.hamburger-return').on('click', function() {
+        // Slide the side nav bar out
+        $('.side-nav-bar').css('transform', 'translateX(-100%)');
+
+        // Show the hamburger-icon and hide the hamburger-return icon
+        $('.hamburger-icon').show();
+        // $('.hamburger-return').hide();
+    });
+
+    // Close the side nav bar when clicked outside
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.side-nav-bar').length && !$(e.target).hasClass('hamburger-icon')) {
+            closeSideNavBar();
+        }
+    });
+
+    function closeSideNavBar() {
+        // Slide the side nav bar out
+        $('.side-nav-bar').css('transform', 'translateX(-100%)');
+
+        // Show the hamburger-icon and hide the hamburger-return icon
+        $('.hamburger-icon').show();
+        // $('.hamburger-return').hide();
+    }
+});
+
+
+// Upon page load, animate the opacity of these text elements to 1.
+$(document).ready(function() {
+    animateTextOpacity('1');
 });
 
 swipeContainer.addEventListener('touchstart', (e) => {

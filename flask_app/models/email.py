051c6558d671ee1d369fe8e8fd3f02add44889be
Andrewjkim2035 <andrewjkim2035@gmail.com>
2023-08-17 23:32:45 -0400

    updated

--- flask_app/models/email.py
+++ flask_app/models/email.py
@@ -12,24 +12,24 @@ msg_text = """
         <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body style="margin-left: 2%;">
-        <p style="opacity: 0; display: hidden; color: #000000;"> </p>
+        <a style="opacity: 0; display: hidden;"> </a>
         <table class="email" width="620" height="815" valign="top" cellpadding="0" cellspacing="0" border="0" style="word-break: break-word;">
             <tr>
                 <td width="725" height="815" background="https://i.imgur.com/lFVoNxz.png" background-repeat="no-repeat" background-size="cover" background-position="center" margin-top: 10%;>
 
-                <p class="email-header" style="height: 12%; width: 90%; text-align: end; margin-bottom: 26.5%; color: #000000;">Ticket #{number}</p>
+                <a class="email-header" style="height: 12%; width: 90%; text-align: end; margin-bottom: 26.5%">Ticket #{number}</a>
 
                 <table cellpadding="0" cellspacing="0" border="0" margin="auto" >
                     <tr>
-                        <p style="text-align: start; width: 75%; height: 20%; margin-left: 12.5%; margin-top: 12%; color: black;">Congratulations {customer}! </p>
+                        <a style="text-align: start; width: 75%; height: 20%; margin-left: 12.5%; margin-top: 12%;">Congratulations {customer}! </a>
                         
-                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; color:#000000 !important;">You are now on the waitlist for LuxChain Wallet Application! Stay tuned for more announcements & updates on the release date.</p>
+                        <a style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; text-decoration: none; border: none; color: #000001">You are now on the waitlist for LuxChain Wallet Application! Stay tuned for more announcements & updates on the release date.</a>
 
-                        <a style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; color: black;">LuxChain is a user-friendly application designed to streamline the transaction process, promoting the adoption and growth of blockchain technology. Especially valuable during economic uncertainty, LuxChain ensures users a high degree of privacy and trust. The application provides a decentralized platform for safe and efficient transactions, transforming the way people purchase items.</a>
+                        <a style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%">LuxChain is a user-friendly application designed to streamline the transaction process, promoting the adoption and growth of blockchain technology. Especially valuable during economic uncertainty, LuxChain ensures users a high degree of privacy and trust. The application provides a decentralized platform for safe and efficient transactions, transforming the way people purchase items.</a>
 
-                        <p style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; ">Have any questions about the application? Please visit the website, www.luxorawallet.io, and fill out the form under contact us!</p>
+                        <a style="text-align: start; width: 75%; height: 20%; padding-left: 12.5%; ">Have any questions about the application? Please visit the website, www.luxorawallet.io, and fill out the form under contact us!</a>
                         
-                        <p style="text-align: start; width: 40%; height: 10%; padding-left: 12.5%; padding-top: 1%;">Our best, <br>LuxChain Team</p>    
+                        <a style="text-align: start; width: 40%; height: 10%; padding-left: 12.5%; padding-top: 1%;">Our best, <br>LuxChain Team</a>    
                     </tr>
                 </table>
                 

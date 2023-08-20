70b56514a3187e87d592227bc80077475511f140
peterkim2014 <peterkim2014@gmail.com>
2023-08-19 20:39:50 -0400

    updated

--- flask_app/models/info.py
+++ flask_app/models/info.py
@@ -1,11 +1,15 @@
-email = "LuxChain <program.python.test@gmail.com>"
-password = "Nova2023!"
-username = "program.python.test@gmail.com"
-app_password = "ujweanmungpxlahc"
-# waitlist_username = "customersupport@luxorawallet.io"
-# waitlist_email = "LuxoraWallet <customersupport@luxorawallet.io>"
-# waitlist_app_password = "rkbbupfowvkgllxg"
+# Test development
+# email = "LuxChain <program.python.test@gmail.com>"
+# password = "Nova2023!"
+# username = "program.python.test@gmail.com"
+# app_password = "ujweanmungpxlahc"
 
+# Waitlist
+username = "waitlist@luxorawallet.io"
+email = "LuxoraWallet <waitlistt@luxorawallet.io>"
+app_password = "pffpsedhxcjpukbj"
+
+# Contact Us
 # contact_username = "luxorawaitlist@luxorawallet.io"
 # contact_email = "LuxoraWaitlist <luxorawaitlist@luxorawallet.io>"
 # contact_app_password = "dkpxvdxhseyrquhp"
